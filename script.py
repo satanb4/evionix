d39a6b6e98b05fb4fd1456de5915ef011ce7acb9
Neha <nandineha09@gmail.com>
2019-03-26 18:36:08 +0530

    Neha

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ def home():
     if request.method == 'POST':
         name = request.form.get("contactName")
         mail = request.form.get("contactEmail")
-        subject = request.form.get("contactEmail")
+        subject = request.form.get("contactSubject")
         message = request.form.get("contactMessage")
     return render_template("index.html", article=Articles)
 
@@ -18,7 +18,16 @@ def home():
 def contact():
     return None
 
+@app.route("/articles/<int:id>/")
+def article(id):
+    try: 
+        return render_template("article.html", id = id, article=Articles[id-1])
+    except Exception:
+        return None 
+
+
+
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
