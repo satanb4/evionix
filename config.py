3f148cffa96678d7124e5c96fc32aa2baca96b28
Sayan Bandyopadhyay <sayan.bandyopadhyay@yahoo.co.in>
2024-03-15 21:49:44 +0000

    Update config.py
    Edited sub route

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     DEBUG = os.environ.get('DEBUG')
-    APPLICATION_ROOT = '/evionix/'
+    APPLICATION_ROOT = '/evionix'
