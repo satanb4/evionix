c29db5a08db707aa52d74ea472a7f1e55cba389d
Sayan Bandyopadhyay <sayan.bandyopadhyay@yahoo.co.in>
2024-03-15 15:53:16 +0000

    Added passenger application

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,4 +2,4 @@ import os, sys
 INTERP = "/usr/bin/python3"
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 print(sys.version)
-from app import app
+from app import app as application
